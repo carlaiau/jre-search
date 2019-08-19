2a7fea18719853c06ac566e59ae70548ee666e51
Carl Aiau <carl@ortus.co.nz>
2019-08-19 23:02:38 +1200

    basic boring af

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <nav class="navbar is-fixed-top">
+    <div class="container">
+      <div class="navbar-brand">
+        <p><strong>JRE</strong> Alpha Search</p>
+      </div>
+      <div class="navbar-end">
+        <form @submit.prevent="runSearch">
+          <div class="field has-addons">
+            <div class="control">
+              <input class="input" type="text" placeholder="Pull that up Jamie" v-model="query">
+            </div>
+            <div class="control">
+              <a class="button is-info" :click="runSearch">
+                Search
+              </a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      query: ''
+    }
+  },
+  methods: {
+    runSearch(event){
+      if(this.query == '')
+        alert("Please Input a search term")
+        return
+      
+    }
   }
 }
+
 </script>
 
 <style>
@@ -24,5 +51,27 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
+nav.navbar{
+  display: block;
+  box-shadow: 0px 1px 2px rgba(0,0,0,0.1);
+}
+nav.navbar p{
+  margin: 0;
+}
+.navbar .container{
+  padding: 10px 0 0; }
+
+.navbar > .container{
+  min-height: 0
+}
+.navbar-brand{
+  padding-right: 20px;
+}
+.navbar-brand p{
+  font-size: 1.5em;
+  line-height: 1.5em;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import 'bulma/css/bulma.css'
 
 new Vue({
   render: h => h(App),
